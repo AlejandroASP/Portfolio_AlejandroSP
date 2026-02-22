ea314d4a0688dd7ee90c8ba62f6e88a9c9fa0e5b
AlejandroASP <alejandroasp71@gmail.com>
2026-02-22 13:34:55 +0000

    Ajuste menu hamburguesa
    Con los cambios hechos anteriormente se dejo de mostrar el menu de hamburguesa

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const nav = document.querySelector('nav ul');
-    const toggle = document.createElement('div');
-    toggle.classList.add('nav-toggle');
-    toggle.innerHTML = '&#9776;';
-    document.querySelector('header').appendChild(toggle);
+    const toggle = document.querySelector('.nav-toggle');
 
     toggle.addEventListener('click', function() {
         nav.classList.toggle('active');
+        toggle.classList.toggle('active');
+
+        if (nav.classList.contains('active')) {
+            toggle.innerHTML = '&#10005;';
+        } else {
+            toggle.innerHTML = '&#9776;';
+        }
     });
 });
